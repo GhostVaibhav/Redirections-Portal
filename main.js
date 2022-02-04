fb8b2c8d565687c1b490a0815dc56af41320866d
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-02-04 23:16:14 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const arr = window.location.href.split('/');
+const site = arr[arr.length - 1].slice(1);
+if (site === "old") {
+    window.location.replace('https://legacy.ghostvaibhav.me');
+}
+else if (site === "blue") {
+    window.location.replace('https://blue.ghostvaibhav.me');
+}
+else if (site === "") {
+    window.location.replace('https://new.ghostvaibhav.me');
+}
