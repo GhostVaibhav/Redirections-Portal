b189de1ecbfcfe813c7387c397665b2bab69c875
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-02-05 00:10:33 +0530

    Added favicons and delay for redirection

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const arr = window.location.href.split('/');
-const site = arr[arr.length - 1].slice(1);
-if (site === "old") {
-    window.location.replace('https://legacy.ghostvaibhav.me');
-}
-else if (site === "blue") {
-    window.location.replace('https://blue.ghostvaibhav.me');
-}
-else if (site === "") {
-    window.location.replace('https://new.ghostvaibhav.me');
-}
+setTimeout(function() {
+    const arr = window.location.href.split('/');
+    const site = arr[arr.length - 1].slice(1);
+    if (site === "old") {
+        window.location.replace('https://legacy.ghostvaibhav.me');
+    }
+    else if (site === "blue") {
+        window.location.replace('https://blue.ghostvaibhav.me');
+    }
+    else if (site === "") {
+        window.location.replace('https://new.ghostvaibhav.me');
+    }
+}, 5000);
