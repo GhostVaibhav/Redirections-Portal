f1f1c5296c331e7f79eb1ad9b4494d04cae0b502
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2022-02-13 16:04:53 +0530

    Added the else condition for popping 404 error

--- main.js
+++ main.js
@@ -12,16 +12,19 @@ else if (site === "") {
     document.getElementById("output").innerHTML = 'https://new.ghostvaibhav.me';
     document.getElementById("output").href = 'https://new.ghostvaibhav.me';
 }
+else {
+    document.getElementById("output").innerHTML = 'https://404.ghostvaibhav.me';
+    document.getElementById("output").href = 'https://404.ghostvaibhav.me';
+}
 setTimeout(function () {
     const arr = window.location.href.split('/');
     const site = arr[arr.length - 1].slice(1);
-    if (site === "old") {
+    if (site === "old")
         window.location.replace('https://legacy.ghostvaibhav.me');
-    }
-    else if (site === "blue") {
+    else if (site === "blue")
         window.location.replace('https://blue.ghostvaibhav.me');
-    }
-    else if (site === "") {
+    else if (site === "")
         window.location.replace('https://new.ghostvaibhav.me');
-    }
+    else
+        window.location.replace("https://404.ghostvaibhav.me");
 }, 2000);
