74a404a0fc521db2155c6571664c3fbfd7beece9
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2023-06-17 17:07:57 +0530

    Updated the TLD of the domains

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const arr = window.location.href.split('/');
 const site = arr[arr.length - 1].slice(1);
 if (site === "old") {
-    document.getElementById("output").innerHTML = 'https://legacy.ghostvaibhav.tech';
-    document.getElementById("output").href = 'https://legacy.ghostvaibhav.tech';
+    document.getElementById("output").innerHTML = 'https://legacy.ghostvaibhav.codes';
+    document.getElementById("output").href = 'https://legacy.ghostvaibhav.codes';
 }
 else if (site === "blue") {
-    document.getElementById("output").innerHTML = 'https://blue.ghostvaibhav.tech';
-    document.getElementById("output").href = 'https://blue.ghostvaibhav.tech';
+    document.getElementById("output").innerHTML = 'https://blue.ghostvaibhav.codes';
+    document.getElementById("output").href = 'https://blue.ghostvaibhav.codes';
 }
 else if (site === "") {
-    document.getElementById("output").innerHTML = 'https://new.ghostvaibhav.tech';
-    document.getElementById("output").href = 'https://new.ghostvaibhav.tech';
+    document.getElementById("output").innerHTML = 'https://new.ghostvaibhav.codes';
+    document.getElementById("output").href = 'https://new.ghostvaibhav.codes';
 }
 else {
-    document.getElementById("output").innerHTML = 'https://404.ghostvaibhav.tech';
-    document.getElementById("output").href = 'https://404.ghostvaibhav.tech';
+    document.getElementById("output").innerHTML = 'https://404.ghostvaibhav.codes';
+    document.getElementById("output").href = 'https://404.ghostvaibhav.codes';
 }
 setTimeout(function () {
     const arr = window.location.href.split('/');
